affb757cbbe4339b27b1f273d6ccd84be295b241
PaweLewoniewski <pawel.lewoniewski@gmail.com>
2022-05-29 12:51:53 +0200

    update_rest_data

--- src/playground/previewView.tsx
+++ src/playground/previewView.tsx
@@ -12,27 +12,34 @@ const PreviewView = () => {
     const { roomData } = useAppSelector((store: RootState) => store.multiReducers.localDataReducer);
     const { restrictions } = useAppSelector((store: RootState) => store.multiReducers.localDataReducer);
     const localData: string | null = localStorage.getItem("roomDim");
+    const restlocalData: string | null = localStorage.getItem("restDim");
     const [loadData, setLoadData] = useState<RoomData[]>();
     const dispatch = useAppDispatch();
+
     useEffect(() => {
         if (localData !== null && loadData === undefined) {
-            const dataObj = JSON.parse(localData);
-            setLoadData(dataObj);
-            dispatch({ type: "ROOM_DIMENSIONS", payload: dataObj });
+            const roomDataObj = JSON.parse(localData);
+            setLoadData(roomDataObj);
+            dispatch({ type: "ROOM_DIMENSIONS", payload: roomDataObj });
+        }
+        if (restlocalData !== null) {
+            const restDataObj = JSON.parse(restlocalData);
+            dispatch({ type: "ADD_RESTRICTIONS", payload: restDataObj });
         }
         if (loadData?.length !== roomData) {
             setLoadData(roomData);
         }
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [loadData, localData]);
-    console.log(roomData)
+
+    console.log(loadData)
     // const roomWidth = loadData.width / 4; //przy /5 to: 200 px to 1 metr
     // const roomDepth = loadData.depth / 4;
     return (
         <>
             {loadData && loadData.length > 0 ? loadData.map((item: RoomData, index) =>
             (<Room key={index} roomWidth={item.roomWidth} roomDepth={item.roomDepth}>
-                {restrictions !== null  ?  <RestrictionBox elementsData={restrictions} />  : ''}
+                {restrictions !== null ? <RestrictionBox elementsData={restrictions} /> : ''}
             </Room>
             )) : 'Set Room Dimensions'}
         </>
