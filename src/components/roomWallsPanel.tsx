3b54704a0afa5216109aa714fe084db56beac869
PaweLewoniewski <pawel.lewoniewski@gmail.com>
2022-06-08 23:44:40 +0200

    prevent_clear

--- src/components/roomWallsPanel.tsx
+++ src/components/roomWallsPanel.tsx
@@ -12,12 +12,16 @@ const RoomWallsPanel = () => {
     const [widthPlayground, setWidthPlayground] = useState<number | undefined>();
     const [depthPlayground, setDepthPlayground] = useState<number | undefined>();
     const [wallDistance, setWallDistance] = useState<number | undefined>(0);
+    const localData: string | null = localStorage.getItem("kitchenData");
 
     const setUpRoomPlayground = () => {
-        const data = [{roomDimension:{roomWidth: widthPlayground,roomDepth: depthPlayground, distance:wallDistance }}]
+        if (localData === null) {
+        const data = [{roomDimension:{roomWidth: widthPlayground, roomDepth: depthPlayground, distance:wallDistance }}]
         localStorage.setItem("kitchenData", JSON.stringify(data));
         dispatch({ type: "ROOM_DIMENSIONS", payload: data });
         navigate('/bottomCabinets');
+        }
+        else return console.log('clear room !')
     }
 
     return (
